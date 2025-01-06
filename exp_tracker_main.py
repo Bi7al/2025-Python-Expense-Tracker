c2b18f86fd266f0074f4d77a3eeb70ea1ae53cd9
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-01-06 21:40:33 +0500

    Added a menu interface and Exit Function

--- exp_tracker_main.py
+++ exp_tracker_main.py
@@ -1,25 +1,47 @@
 import datetime;
 
-print("""Hello to Expense Tracker""")
-print("-------------------------------")
-expenses = [];
-Expense = {
-    "title":"",
-    "cost":0.0,
-    "date": datetime,
-    "desc":""
-}
-def add_new_expense():
-    Expense["title"] = input("Enter expense title: ");
-    Expense["cost"]= input("Enter Cost : ");
-    Expense["desc"]=input("Enter Description : ");
-    Expense["date"]= datetime.datetime.now();
-    expenses.append(Expense.copy());
-add_new_expense();
 
+expenses = [];
+
+def add_new_expense():
+    Expense = {
+    "title":input("Enter expense title: "),
+    "cost":input("Enter Cost : "),
+    "date": datetime.datetime.now(),
+    "desc":input("Enter Description : ")
+    }
+    expenses.append(Expense);
+    Expense={};
+    print("Expense Added Successfully!!\n")
+# End of Add new Expense Function    
+    
+    
 
 def print_expenses():
     for i in range(len(expenses)):
-        print(f"""Expense Title : {expenses[i]["title"]}   Dated : {str(expenses[i]["date"].date())}\n
-Cost : {expenses[i]["cost"]}\nExpense Description : {expenses[i]["desc"]}""")
-print_expenses();
+        print(f"""
+Expense Title : {expenses[i]["title"]}   Dated : {str(expenses[i]["date"].date())}
+Cost : {expenses[i]["cost"]}
+Expense Description : {expenses[i]["desc"]}\n""")
+        
+# End of Print Function        
+        
+
+continue_loop = True;
+print("""Hello to Expense Tracker""");
+print("-------------------------------");
+while(continue_loop):
+    print("""1. Add a New Expense
+2. View Expenses
+3. Exit""");
+    choice = int(input("Enter your choice: "));
+    if(choice==1):
+        add_new_expense();
+    elif(choice==2):
+        print_expenses();
+    elif(choice==3):
+        exit();
+    else:
+        print("Invalid Input");
+
+
